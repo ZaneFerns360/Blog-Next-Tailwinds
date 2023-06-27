1710c5f9fbbd4a9f3504fc016281bf951245644c
Zane Fernandes <zane.ferns360@gmail.com>
2023-06-27 23:46:39 +0530

    Basic layout

---
+++ components/Article.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const Article = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Article
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,18 +7,26 @@ import { useEffect, useState } from "react";
 
 const Nav = () => {
   return (
-        <nav className='flex-between gap-x-8 w-full mb-16 pt-3'>
-        <Link href='/' className='flex gap-2 flex-center'>
+        <nav className='flex-between gap-x-1 w-full mb-16 pt-3'>
+        <Link href='/' className='flex gap-2 flex-center max-sm:hidden'>
         <Image
           src='/assets/logo.svg'
           alt='logo'
-          width={100}
+          width={130}
           height={100}
           className='object-contain'
         />
-        <p className='logo_text'>DEVCUT</p>
+        
       </Link>
-
+      <div className='flex flex-center'>
+        <form className='w-96 flex max-sm:w-60'>
+          <input type='text' className='search_input' placeholder='Search for'/>
+          </form>
+          </div>
+          <button className='rounded-full border border-black bg-black py-2 px-5
+           text-white transition-all hover:bg-white
+            hover:text-black text-center text-sm font-inter
+             flex items-center justify-center'>About Me</button>
         </nav>
 
   )
