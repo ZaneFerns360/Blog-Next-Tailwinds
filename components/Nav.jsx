e08e7e58d19d5f5952f8a25774bb3ac2072f3df2
Zane Fernandes <zane.ferns360@gmail.com>
2023-07-12 16:13:03 +0530

    minor styling

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 const Nav = () => {
   return (
-        <nav className='flex-between gap-x-1 w-full mb-2 mt-4 max-sm:gap-x-0 min-w-[371px]'>
+        <nav className='flex-between gap-x-1 w-full mb-2 mt-4 max-sm:gap-x-0 min-w-[360px]'>
         <Link href='/' className='flex gap-2 flex-center pl-1 max-sm:gap-0'>
         <Image
           src='/assets/logo-no-background.svg'
